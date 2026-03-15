adc65e2a2acb413458377496bae9bff2a8b9c469
zelig53 <zelig5362@gmail.com>
2026-03-15 13:49:41 +0200

    Update vite.config.ts

--- app-fixed/vite.config.ts
+++ app-fixed/vite.config.ts
@@ -48,9 +48,6 @@ export default defineConfig(({mode}) => {
         }
       })
     ],
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
